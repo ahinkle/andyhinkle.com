92df188bded7cf9ec1d4441d2c1553a5b41baded
Andy Hinkle <ahinkle10@gmail.com>
2024-05-22 20:30:43 -0500

    wip

--- resources/views/components/layout/navigation.blade.php
+++ resources/views/components/layout/navigation.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center flex-wrap gap-2 mb-8 group">
     <x-layout.navigation-item href="{{ url('/') }}">About</x-layout.navigation-item>
     <x-layout.navigation-item href="{{ url('/gear') }}">Gear</x-layout.navigation-item>
-    <x-layout.navigation-item href="{{ url('/blog') }}">Contact</x-layout.navigation-item>
+    <x-layout.navigation-item href="{{ url('/contact') }}">Contact</x-layout.navigation-item>
 </div>
--- resources/views/pages/gear/index.blade.php
+++ resources/views/pages/gear/index.blade.php
@@ -1,8 +1,12 @@
 <x-app>
     <x-slot name="seo">
-        <title>Andy Hinkle | Software Developer</title>
-        <meta name="description" content="Andy Hinkle is a software developer with over 10 years of experience specializing in Laravel, PHP, and JavaScript.">
+        <title>Andy Hinkle | Uses, Gear</title>
+        <meta name="description" content="This is my daily uses for developing software. I use a variety of tools and software to help me develop software.">
     </x-slot>
 
-
+    <div class="flex flex-col items-center justify-center">
+        <h1 class="text-2xl font-bold mb-4 text-center text-gray-900 dark:text-gray-100">
+            Coming Soon!
+        </h1>
+    </div>
 </x-app>
