e81da2b66841ce648d2612cd552bfeaae7716d98
Andy Hinkle <ahinkle10@gmail.com>
2024-05-24 21:30:32 -0500

    gear page

--- resources/views/pages/gear/index.blade.php
+++ resources/views/pages/gear/index.blade.php
@@ -4,9 +4,100 @@
         <meta name="description" content="This is my daily uses for developing software. I use a variety of tools and software to help me develop software.">
     </x-slot>
 
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-2xl font-bold mb-4 text-center text-gray-900 dark:text-gray-100">
-            Coming Soon!
-        </h1>
-    </div>
+    <h1 class="text-3xl font-semibold text-gray-800 dark:text-gray-100">Gear / Uses</hjson>
+    <section class="md:border-l md:border-gray-500 md:pl-6 md:dark:border-gray-250/40 mt-10">
+        <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+            <h2 class="text-sm font-semibold text-gray-800 dark:text-gray-100">Hardware</h2>
+            <div class="md:col-span-3">
+                <ul role="list" class="space-y-10">
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">16” MacBook Pro, M3 Max, 36GB RAM</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Space Black, 1TB SSD</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">49" Samsung Odyssey G9</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">5120x1440, 32:09, 240Hz</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Razer DeathAdder V2 Mouse</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">20,000 DPI, 7 Buttons</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Keychron K3 Mechanical Keyboard</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">75% Layout, Gateron Red Switches</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Bose QuietComfort 35 II Headphones</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Noise Canceling, Black</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Sudotack ST-800 Microphone</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">24-bit, 192kHz, Cardioid</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Canon EOS Rebel T7</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Paired with Elgato Cam Link, Webcam</div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="md:border-l md:border-gray-500 md:pl-6 md:dark:border-gray-250/40 mt-20">
+        <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+            <h2 class="text-sm font-semibold text-gray-800 dark:text-gray-100">Development</h2>
+            <div class="md:col-span-3">
+                <ul role="list" class="space-y-10">
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Visual Studio Code</h3>
+                        <div>
+                            <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
+                                Font: <a href="https://www.monolisa.dev/" target="_blank" rel="noopener noreferrer" class="dark:text-gray-200 text-gray-800 underline">MonoLisa</a>
+                            </div>
+                            <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
+                                Theme: <a href="https://marketplace.visualstudio.com/items?itemName=this-fifo.natty" target="_blank" rel="noopener noreferrer" class="dark:text-gray-200 text-gray-800 underline">Natty</a>
+                            </div>
+                            <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
+                                Settings: <a href="https://gist.github.com/ahinkle/d99742b14dab934300e7120887420bbd" target="_blank" rel="noopener noreferrer" class="dark:text-gray-200 text-gray-800 underline">settings.json</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">TablePlus</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">MySQL, PostgreSQL, Redis</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Insomnia</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">API Testing, GraphQL</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Laravel Herd</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Local Development</div>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">GitHub</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Version Control, Code Review</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="md:border-l md:border-gray-500 md:pl-6 md:dark:border-gray-250/40 mt-20 mb-10">
+        <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+            <h2 class="text-sm font-semibold text-gray-800 dark:text-gray-100">Design</h2>
+            <div class="md:col-span-3">
+                <ul role="list" class="space-y-10">
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Figma</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">UI/UX Design, Prototyping</div>
+                    </li>
+                    <li class="group relative flex flex-col items-start">
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Adobe Creative Cloud</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Photoshop, Illustrator, XD</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </x-app>
