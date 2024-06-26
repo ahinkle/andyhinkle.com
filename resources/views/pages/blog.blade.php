4fec81e470577017dc98357af8aa3d31336a9a80
Andy Hinkle <ahinkle10@gmail.com>
2024-06-25 21:56:29 -0500

    wip

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,3 +1,6 @@
-<div>
-    //
-</div>
+<x-app>
+    <x-slot name="seo">
+        <title>Blog | Andy Hinkle</title>
+        <meta name="description" content="A collection of articles on web development, Laravel, and more.">
+    </x-slot>
+</x-app>
