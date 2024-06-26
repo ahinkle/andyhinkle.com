69e831a790d7ae3946bc557257969274f68480fc
Andy Hinkle <ahinkle10@gmail.com>
2024-06-25 20:59:48 -0500

    wip

---
+++ resources/views/pages/blog/just-try-it.blade.php
@@ -0,0 +1,8 @@
+<x-app>
+    <x-slot name="seo">
+        <title>Just Try It</title>
+        <meta name="description" content="If something makes you feel uncomfortable, just try it.">
+    </x-slot>
+
+    
+</x-app>
