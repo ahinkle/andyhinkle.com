24372c49e49eac56e25f880895af53346e611e7e
Andy Hinkle <ahinkle10@gmail.com>
2024-06-25 21:45:42 -0500

    wip

--- resources/views/pages/blog/just-try-it.blade.php
+++ resources/views/pages/blog/just-try-it.blade.php
@@ -4,5 +4,105 @@
         <meta name="description" content="If something makes you feel uncomfortable, just try it.">
     </x-slot>
 
-    
+    <p class="text-white/75 text-center text-sm py-2 font-sans">June 25, 2024</p>
+    <h1 class="text-5xl font-bold text-white text-center font-sans">Just Try It</h1>
+
+    <div class="max-w-3xl mx-auto mt-8">
+        <p class="text-white/85 text-lg text-left">
+            We often find comfort in the familiar. The frameworks, libraries, and patterns we've used for years become our go-to tools.
+            However, this comfort can sometimes turn into a reluctance to explore new packages or patterns, leading to missed opportunities for growth and innovation. 
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            When faced with a new technology or pattern that makes you feel uncomfortable, <span class="font-bold">just try it</span>. You may find that it opens up new possibilities and ways of thinking that you hadn't considered before.
+        </p>
+
+        <h2 class="text-2xl text-white font-bold mt-8 text-left">
+            Tailwind CSS
+        </h2>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            When I first heard about Tailwind CSS, I was skeptical. The idea of writing inline styles in my HTML felt wrong, and I dismissed it as a fad that would soon pass.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I never gave it a fair chance. I quickly dismissed it because it made me feel uncomfortable. 
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            However, after seeing the positive feedback from other developers and everyone who was using it absolutely loved it, I decided to give it a try.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I was wrong.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            Tailwind CSS has become my go-to tool for styling web applications. It has allowed me to build beautiful, responsive designs quickly and efficiently, and I can't imagine going back to traditional CSS.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            Initially, my team was reluctant to use it. They were comfortable with their existing tools (e.g. SASS) and didn't see the value in trying something new. I didn't push the issue but instead used Tailwind CSS on greenfield projects
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I didn't push the issue, but I instead starting using it on greenfield projects. After seeing the positive result and contributing to the project, they started to see the value in it and eventually adopted it as well.
+        </p>
+
+        <h2 class="text-2xl text-white font-bold mt-8 text-left">
+            Ignore the Naysayers
+        </h2>
+        <p class="text-white/85 text-lg text-left mt-4">
+           I'm a moderator at <a href="https://reddit.com/r/laravel" class="text-blue-500/75">/r/laravel</a>, and I can assure you that when a new package or pattern is introduced, there are always naysayers who dismiss it without giving it a fair chance.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            There will always be detractors who are quick to label new tools as "bad" or "wrong" because they don't fit their existing mental model.
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            Ignore these voices and form your own opinions based on your experiences and the feedback from trusted peers in your developer community.
+        </p>
+
+        <h2 class="text-2xl text-white font-bold mt-8 text-left">
+            PestPHP
+        </h2>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I was first introduced to PestPHP at <a href="https://www.youtube.com/watch?v=AkDMDHAs09U" target="_blank" class="text-blue-500/75" rel="noopener">
+                Laracon US 2023
+            </a>
+            in a talk by Nuno Maduro. I was intrigued by the syntax and the promise of faster, more readable tests, but I was skeptical.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I was with a co-worker and we both just didn't get it. We were comfortable with PHPUnit and didn't see the value in trying something new.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            It felt like semantic sugar and didn't see the value in it. I didn't give it a chance and was quick to dismiss it.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            However, I kept seeing positive feedback from other developers and thought, "What the heck, I'll give it a try."
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            I was wrong.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            PestPHP is incredibly clean, like a breath of fresh air. What I didn't realize initially is that it's more than just semantics. It's easier to read and write, and I found myself writing tests faster than ever before.
+        <p>
+
+        <h2 class="text-2xl text-white font-bold mt-8 text-left">
+            So Just Try It
+        </h2>
+
+        <p class="text-white/85 text-lg text-left mt-4">
+            Get your hands dirty. If something makes you feel uncomfortable, just try it. You may find that it opens up new possibilities and ways of thinking that you hadn't considered before. Don't be so dismissive even if it feels wrong.
+        </p>
+
+        <p class="text-white/85 text-lg text-left mt-4 pb-4">
+            Cheers
+        </p>
 </x-app>
