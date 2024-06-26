16d0507c5d79177e460f151f2fb5ac896625f98c
Andy Hinkle <ahinkle10@gmail.com>
2024-06-26 09:33:16 -0500

    wip

--- app/View/Components/BlogListing.php
+++ app/View/Components/BlogListing.php
@@ -22,28 +22,44 @@ class BlogListing extends Component
         return Cache::rememberForever('post-listing', function () {
             return collect(glob(resource_path('views/pages/blog/*.blade.php')))
                 ->map(function ($file) {
-                    $slug = str_replace('.blade.php', '', pathinfo($file, PATHINFO_BASENAME));
                     $content = file_get_contents($file);
 
-                    preg_match('/<title>(.*?)<\/title>/', $content, $matches);
-                    $title = $matches[1] ?? '';
-
-                    preg_match('/<p\b[^>]*>(.*?)<\/p>/', $content, $matches);
-                    $date = $matches[1] ?? '';
-
-                    preg_match('/<p\b[^>]*>([^<]*?[^<]{20,}.*?)<\/p>/', $content, $matches);
-                    $description = $matches[1] ?? '';
-                    $description = preg_replace('/\s+/', ' ', $description);
-
                     return [
-                        'path' => $file,
-                        'slug' => '/blog/'.$slug,
-                        'title' => $title,
-                        'description' => $description,
-                        'date' => $date,
+                        'slug' => $this->resolveSlug($file),
+                        'title' => $this->resolveTitle($content),
+                        'description' => $this->resolveDescription($content),
+                        'date' => $this->resolvePublishedDate($content),
                     ];
                 })
                 ->sortByDesc('date');
         });
+    }
+
+    protected function resolveSlug(string $file): string
+    {
+        $slug = str_replace('.blade.php', '', pathinfo($file, PATHINFO_BASENAME));
+
+        return '/blog/'.$slug;
+    }
+
+    protected function resolveTitle(string $content): string
+    {
+        preg_match('/<title>(.*?)<\/title>/', $content, $matches);
+
+        return $matches[1] ?? '';
+    }
+
+    protected function resolvePublishedDate(string $content): string
+    {
+        preg_match('/<p\b[^>]*>(.*?)<\/p>/', $content, $matches);
+
+        return $matches[1] ?? '';
+    }
+
+    protected function resolveDescription(string $content): string
+    {
+        preg_match('/<p\b[^>]*>([^<]*?[^<]{20,}.*?)<\/p>/', $content, $matches);
+
+        return $matches[1] ?? '';
     }
 }
