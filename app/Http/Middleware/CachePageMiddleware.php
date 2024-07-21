4217356885ce4970f0c7f45e388467b454d33fbd
Andy Hinkle <ahinkle10@gmail.com>
2024-07-20 21:39:25 -0500

    wip

---
+++ app/Http/Middleware/CachePageMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Symfony\Component\HttpFoundation\Response;
+
+class CachePageMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($this->shouldCacheResponse($request, $response)) {
+            $response->headers->add([
+                'Cache-Control' => 'max-age=1800, public',
+            ]);
+        }
+
+        return $response;
+    }
+
+    public function shouldCacheResponse($request, Response $response): bool
+    {
+        if (! app()->isProduction()) {
+            return false;
+        }
+
+        if (auth()->check()) {
+            return false;
+        }
+
+        if (! $request->isMethod('GET')) {
+            return false;
+        }
+
+        if (! $response->isSuccessful()) {
+            return false;
+        }
+
+        return true;
+    }
+}
