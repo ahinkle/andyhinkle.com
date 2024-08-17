3c6eb1004ea9ce1309ca98232258192e01ad9948
Andy Hinkle <ahinkle10@gmail.com>
2024-08-17 09:32:14 -0500

    wip

---
+++ resources/views/pages/blog/we-started-a-podcast.blade.php
@@ -0,0 +1,43 @@
+<x-app>
+    <x-slot name="seo">
+        <title>We started a podcast</title>
+        <meta name="description" content="We started a Laravel podcast to share our experiences and insights with the community." />
+        <meta property="og:title" content="We started a podcast" />
+        <meta property="og:description" content="We started a Laravel podcast to share our experiences and insights with the community." />
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                "headline": "We started a podcast",
+                "datePublished": "2024-08-16T09:00:00+08:00",
+                "dateModified": "2024-08-16T09:00:00+08:00",
+            }
+        </script>
+    </script>
+    </x-slot>
+
+    <p class="text-white/75 text-center text-sm py-2 font-sans">August 16, 2024</p>
+    <h1 class="text-3xl md:text-5xl font-bold text-white text-center font-sans">We started a podcast</h1>
+
+    <div class="max-w-3xl mx-auto mt-8">
+        <p class="text-white/85 text-lg text-left text-wrap">
+            My good friend <a href="https://daltonmccleery.com" class="text-blue-400 hover:underline">Dalton McCleery</a> and I have had countless deep-dive conversations over Slack about Laravel, PHP, and all things web development. We've been toying with the idea of starting a podcast for a while now, and we finally made it happen! 
+        </p>
+
+        <p class="text-white/85 text-lg text-left text-wrap mt-4">
+            Introducing <a href="https://the-midwest-artisan.transistor.fm/" class="italic hover:underline hover:text-blue-400" target="_blank" rel="noopener noreferrer">The Midwest Artisan</a> – a podcast where we explore the world of Laravel and web development, and share the knowledge we've gathered over the past decade.
+        </p>
+
+        <p class="text-white/85 text-lg text-left text-wrap mt-4">
+           Laravel has always been a passion of ours, and we've been fortunate to be part of such a vibrant and welcoming community. Through The Midwest Artisan, we want to give back by sharing our experiences, tips, and successes with others who share our love for this incredible framework.
+        </p>
+
+        <p class="text-white/85 text-lg text-left text-wrap mt-4">
+            In each episode, we’ll dive into topics like Laravel, PHP, web development, and more. You can expect practical tips, insights from our projects, and even some guest appearances from fellow developers. Our goal is to inspire and contribute to the Laravel community that has given us so much.
+        </p>
+
+        <div class="px-4 pt-10">
+            <iframe width="100%" height="180" frameborder="no" scrolling="no" seamless="" src="https://share.transistor.fm/e/439210d0?color=1F2937&background=101827"></iframe> 
+        </div>
+    </div>
+</x-app>
