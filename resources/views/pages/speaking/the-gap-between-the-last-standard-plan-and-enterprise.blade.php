749a5220c14494e7766190eaab968c24117f9090
Andy Hinkle <ahinkle10@gmail.com>
2024-08-26 07:50:05 -0500

    wip

---
+++ resources/views/pages/speaking/the-gap-between-the-last-standard-plan-and-enterprise.blade.php
@@ -0,0 +1,20 @@
+<x-app>
+    <x-slot name="seo">
+        <title>The gap between the last Standard Plan and Enterprise | The Midwest Artisan Podcast</title>
+        <meta name="description" content="We dive into A/B testing and the challenges that come with the jump from the highest non-enterprise plan to full enterprise. We also discuss some Laravel shop talk, sharing the tools we've been using recently. ">
+        <meta property="og:title" content="The gap between the last Standard Plan and Enterprise | The Midwest Artisan Podcast" />
+        <meta property="og:description" content="We dive into A/B testing and the challenges that come with the jump from the highest non-enterprise plan to full enterprise. We also discuss some Laravel shop talk, sharing the tools we've been using recently. ">
+    </x-slot>
+
+    <p class="text-white/75 text-center text-sm py-2 font-sans">August 19th, 2024</p>
+    <h1 class="text-3xl md:text-5xl font-bold text-white text-center font-sans">The gap between the last Standard Plan and Enterprise</h1>
+
+    <div class="max-w-3xl mx-auto mt-8">
+        <div class="px-4 pb-10">
+            <iframe width="100%" height="180" frameborder="no" scrolling="no" seamless="" src="https://share.transistor.fm/e/d3f404a8?color=1F2937&background=101827"></iframe> 
+        </div>
+
+        <div class="px-4 prose prose-lg dark:prose-invert">
+            <div>This week, we dive into A/B testing and the challenges that come with the jump from the highest non-enterprise plan to full enterprise. We also discuss some Laravel shop talk, sharing the tools we've been using recently. Plus, Andy introduces his new podcast idea, where he plans to interview Laravel community members and explore their unique development journeys.<br><br>Podcast artwork mentioned on the show is by Robert Wade, robertcreates.com<br><br>Connect with us:<br>Andy Hinkle - X/<a href=\"https://x.com/andyhnk\">@andyhnk</a><br>Dalton McCleery - X/<a href=\"https://x.com/DaltonMcCleery\">@DaltonMcCleery</a></div><ul><li>(00:00) - Intro</li><li>(00:19) - How's it going?</li><li>(01:19) - Our new podcast art cover</li><li>(03:32) - A/B Testing - Bring a fat wallet</li><li>(08:15) - The large gap between the last plan and enterprise is frustrating</li><li>(13:52) - Artisan's Journey - Andy's Podcast Idea</li><li>(28:31) - Laravel Tools</li><li>(39:15) - Is reusing Red Solo cups a boomer Midwest thing?</li><li>(41:13) - Underrated Laravel Stuff Lately</li><li>(52:44) - How do you retroactively sync records with third-party APIs?</li><li>(01:06:00) - Wrap + Outro</li></ul>
+        </div>
+</x-app>
