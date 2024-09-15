56d777e9b469118954b00b517b345162acc624d2
Andy Hinkle <ahinkle10@gmail.com>
2024-09-14 20:14:59 -0500

    wip

--- resources/views/pages/blog/we-started-a-podcast.blade.php
+++ resources/views/pages/blog/we-started-a-podcast.blade.php
@@ -1,9 +1,11 @@
 <x-app>
     <x-slot name="seo">
         <title>We started a podcast</title>
-        <meta name="description" content="We started a Laravel podcast to share our experiences and insights with the community." />
+        <meta name="description"
+            content="We started a Laravel podcast to share our experiences and insights with the community." />
         <meta property="og:title" content="We started a podcast" />
-        <meta property="og:description" content="We started a Laravel podcast to share our experiences and insights with the community." />
+        <meta property="og:description"
+            content="We started a Laravel podcast to share our experiences and insights with the community." />
         <script type="application/ld+json">
             {
                 "@context": "https://schema.org",
@@ -13,7 +15,7 @@
                 "dateModified": "2024-08-16T09:00:00+08:00",
             }
         </script>
-    </script>
+        </script>
     </x-slot>
 
     <p class="text-white/75 text-center text-sm py-2 font-sans">August 16, 2024</p>
@@ -21,23 +23,33 @@
 
     <div class="max-w-3xl mx-auto mt-8">
         <div class="px-4 pb-10">
-            <iframe width="100%" height="180" frameborder="no" scrolling="no" seamless="" src="https://share.transistor.fm/e/439210d0?color=1F2937&background=101827"></iframe> 
+            <iframe width="100%" height="180" frameborder="no" scrolling="no" seamless=""
+                src="https://share.transistor.fm/e/439210d0?color=1F2937&background=101827"></iframe>
         </div>
 
         <p class="text-white/85 text-lg text-left text-wrap">
-            My good friend <a href="https://daltonmccleery.com" class="text-blue-400 hover:underline">Dalton McCleery</a> and I have had countless deep-dive conversations over Slack about Laravel, PHP, and all things web development. We've been toying with the idea of starting a podcast for a while now, and we finally made it happen! 
+            My good friend <a href="https://daltonmccleery.com" class="text-blue-400 hover:underline">Dalton
+                McCleery</a> and I have had countless deep-dive conversations over Slack about Laravel, PHP, and all
+            things web development. We've been toying with the idea of starting a podcast for a while now, and we
+            finally made it happen!
         </p>
 
         <p class="text-white/85 text-lg text-left text-wrap mt-4">
-            Introducing <a href="https://the-midwest-artisan.transistor.fm/" class="italic underline text-blue-400" target="_blank" rel="noopener noreferrer">The Midwest Artisan</a> – a podcast where we explore the world of Laravel and web development, and share the knowledge we've gathered over the past decade.
+            Introducing <a href="https://the-midwest-artisan.transistor.fm/" class="italic underline text-blue-400"
+                target="_blank" rel="noopener noreferrer">The Midwest Artisan</a> – a podcast where we explore the world
+            of Laravel and web development, and share the knowledge we've gathered over the past decade.
         </p>
 
         <p class="text-white/85 text-lg text-left text-wrap mt-4">
-           Laravel has always been a passion of ours, and we've been fortunate to be part of such a vibrant and welcoming community. Through The Midwest Artisan, we want to give back by sharing our experiences, tips, and successes with others who share our love for this incredible framework.
+            Laravel has always been a passion of ours, and we've been fortunate to be part of such a vibrant and
+            welcoming community. Through The Midwest Artisan, we want to give back by sharing our experiences, tips, and
+            successes with others who share our love for this incredible framework.
         </p>
 
         <p class="text-white/85 text-lg text-left text-wrap mt-4">
-            In each episode, we’ll dive into topics like Laravel, PHP, web development, and more. You can expect practical tips, insights from our projects, and even some guest appearances from fellow developers. Our goal is to inspire and contribute to the Laravel community that has given us so much.
+            In each episode, we’ll dive into topics like Laravel, PHP, web development, and more. You can expect
+            practical tips, insights from our projects, and even some guest appearances from fellow developers. Our goal
+            is to inspire and contribute to the Laravel community that has given us so much.
         </p>
     </div>
 </x-app>
--- resources/views/pages/gear.blade.php
+++ resources/views/pages/gear.blade.php
@@ -1,42 +1,57 @@
 <x-app>
     <x-slot name="seo">
         <title>Andy Hinkle | Uses, Gear</title>
-        <meta name="description" content="This is my daily uses for developing software. I use a variety of tools and software to help me develop software.">
+        <meta name="description"
+            content="This is my daily uses for developing software. I use a variety of tools and software to help me develop software.">
     </x-slot>
 
-    <h1 class="text-3xl font-semibold text-gray-800 dark:text-gray-100">Gear / Uses</hjson>
+    <h1 class="text-3xl font-semibold text-gray-800 dark:text-gray-100">Gear / Uses</h1>
     <section class="md:border-l md:border-gray-500 md:pl-6 md:dark:border-gray-250/40 mt-10">
         <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
             <h2 class="text-sm font-semibold text-gray-800 dark:text-gray-100">Hardware</h2>
             <div class="md:col-span-3">
                 <ul role="list" class="space-y-10">
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">16” MacBook Pro, M3 Max, 36GB RAM</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Space Black, 1TB SSD</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">16”
+                            MacBook Pro, M3 Max, 36GB RAM</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Space Black,
+                            1TB SSD</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">49" Samsung Odyssey G9</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">5120x1440, 32:09, 240Hz</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">49"
+                            Samsung Odyssey G9</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">5120x1440,
+                            32:09, 240Hz</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Razer DeathAdder V2 Mouse</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">20,000 DPI, 7 Buttons</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Razer
+                            DeathAdder V2 Mouse</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">20,000 DPI,
+                            7 Buttons</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Keychron K3 Mechanical Keyboard</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">75% Layout, Gateron Red Switches</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Keychron
+                            K3 Mechanical Keyboard</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">75% Layout,
+                            Gateron Red Switches</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Bose QuietComfort 35 II Headphones</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Noise Canceling, Black</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Bose
+                            QuietComfort 35 II Headphones</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Noise
+                            Canceling, Black</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Sudotack ST-800 Microphone</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">24-bit, 192kHz, Cardioid</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Sudotack
+                            ST-800 Microphone</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">24-bit,
+                            192kHz, Cardioid</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Canon EOS Rebel T7</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Paired with Elgato Cam Link, Webcam</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Canon
+                            EOS Rebel T7</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Paired with
+                            Elgato Cam Link, Webcam</div>
                     </li>
 
                 </ul>
@@ -50,38 +65,59 @@
             <div class="md:col-span-3">
                 <ul role="list" class="space-y-10">
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Visual Studio Code</h3>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Visual
+                            Studio Code</h3>
                         <div>
                             <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
-                                Font: MonoLisa <a href="https://www.monolisa.dev/" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a>
+                                Font: MonoLisa <a href="https://www.monolisa.dev/" target="_blank"
+                                    rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a>
                             </div>
                             <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
-                                Theme: Natty <a href="https://marketplace.visualstudio.com/items?itemName=this-fifo.natty" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a>
+                                Theme: Natty <a
+                                    href="https://marketplace.visualstudio.com/items?itemName=this-fifo.natty"
+                                    target="_blank" rel="noopener noreferrer"
+                                    class="dark:text-blue-500 underline text-xs">(link)</a>
                             </div>
                             <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">
-                                Settings: <a href="https://gist.github.com/ahinkle/d99742b14dab934300e7120887420bbd" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">settings.json</a>
+                                Settings: <a href="https://gist.github.com/ahinkle/d99742b14dab934300e7120887420bbd"
+                                    target="_blank" rel="noopener noreferrer"
+                                    class="dark:text-blue-500 underline text-xs">settings.json</a>
                             </div>
                         </div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">TablePlus <a href="https://tableplus.com/" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a></h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">MySQL, PostgreSQL, Redis</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">
+                            TablePlus <a href="https://tableplus.com/" target="_blank" rel="noopener noreferrer"
+                                class="dark:text-blue-500 underline text-xs">(link)</a></h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">MySQL,
+                            PostgreSQL, Redis</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Insomnia <a href="https://insomnia.rest/" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a></h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">API Testing, GraphQL</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Insomnia
+                            <a href="https://insomnia.rest/" target="_blank" rel="noopener noreferrer"
+                                class="dark:text-blue-500 underline text-xs">(link)</a>
+                        </h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">API Testing,
+                            GraphQL</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Laravel Herd</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Local Development</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Laravel
+                            Herd</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Local
+                            Development</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Spatie Ray <a href="https://spatie.be/products/ray" target="_blank" rel="noopener noreferrer" class="dark:text-blue-500 underline text-xs">(link)</a></h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Local Debugging, Logging</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Spatie
+                            Ray <a href="https://spatie.be/products/ray" target="_blank" rel="noopener noreferrer"
+                                class="dark:text-blue-500 underline text-xs">(link)</a></h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Local
+                            Debugging, Logging</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">GitHub</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Version Control, Code Review</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">GitHub
+                        </h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Version
+                            Control, Code Review</div>
                     </li>
                 </ul>
             </div>
@@ -94,12 +130,16 @@
             <div class="md:col-span-3">
                 <ul role="list" class="space-y-10">
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Figma</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">UI/UX Design, Prototyping</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Figma
+                        </h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">UI/UX
+                            Design, Prototyping</div>
                     </li>
                     <li class="group relative flex flex-col items-start">
-                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Adobe Creative Cloud</h3>
-                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Photoshop, Illustrator, XD</div>
+                        <h3 class="text-base font-semibold tracking-tight text-gray-800 dark:text-gray-100">Adobe
+                            Creative Cloud</h3>
+                        <div class="relative z-10 mt-2 text-sm text-gray-600 dark:text-gray-400 w-full">Photoshop,
+                            Illustrator, XD</div>
                     </li>
                 </ul>
             </div>
