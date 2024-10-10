35394fdb79e3b441454cfab24186686219ebd89c
Andy Hinkle <ahinkle10@gmail.com>
2024-10-10 12:50:57 -0500

    wip

--- resources/views/components/recent-podcast-listing.blade.php
+++ resources/views/components/recent-podcast-listing.blade.php
@@ -3,8 +3,13 @@
         <a class="bg-gray-900 p-4 rounded-lg group" href="{{ url($podcast['slug']) }}">
             <div class="grid grid-cols-1 gap-4">
                 <div class="col-span-1">
+                    <div class="flex items-center justify-between relative">
                     <img src="{{ $podcast['youtube_thumbnail'] }}" alt="{{ $podcast['title'] }}"
                         class="rounded-lg w-full h-full object-cover">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-full h-14 fill-current text-white absolute">
+                            <path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9l0 176c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/>
+                        </svg>
+                    </div>
                 </div>
                 <div class="col-span-1">
                     <div class="flex flex-col h-full">
