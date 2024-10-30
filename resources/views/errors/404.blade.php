7577d7205a003d2d3f84bdce43e9b650534be1cf
Andy Hinkle <ahinkle10@gmail.com>
2024-10-30 07:34:47 -0500

    wip

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<x-app>
+    <x-slot name="seo">
+        <title>404</title>
+    </x-slot>
+
+    <div class="container mx-auto px-4 py-8 text-white">
+        <h1 class="text-3xl font-bold text-center">404: Not Found</h1>
+        <p class="text-center mt-4 min-h-screen">
+            Whoops, looks like something went wrong. Please try again or
+            <a href="/" class="text-blue-500 hover:underline">return to the home page.</a>
+        </p>
+    </div>
+</x-app>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+<x-app>
+    <x-slot name="seo">
+        <title>500</title>
+    </x-slot>
+
+    <div class="container mx-auto px-4 py-8 text-white">
+        <h1 class="text-3xl font-bold text-center">500: Server Error</h1>
+        <p class="text-center mt-4 min-h-screen">
+            Whoops, looks like something went wrong. Please try again or
+            <a href="/" class="text-blue-500 hover:underline">return to the home page.</a>
+        </p>
+    </div>
+</x-app>
