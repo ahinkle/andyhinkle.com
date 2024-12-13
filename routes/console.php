b695ad6e685c340d3523c78b8c5083bf9bafdbac
Andy Hinkle <ahinkle10@gmail.com>
2024-12-13 10:39:56 -0600

    wip

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('fetch:github-contributions')->daily();
 
-Schedule::job(new \App\Jobs\DebugCarbonTimingJob)->everyMinute();
+Schedule::job(\App\Jobs\DebugCarbonTimingJob::class)->everyMinute();
