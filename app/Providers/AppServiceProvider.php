b1eeacbadd7749610e7a7d001312f51ed703b845
ahinkle <ahinkle@users.noreply.github.com>
2024-12-18 03:25:14 +0000

    Fixes coding style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void {
+    public function register(): void
+    {
         //
     }
 
