6d1ca447c943f2f2079a462128d7ad0574c7b780
ahinkle <ahinkle10@gmail.com>
2025-03-28 21:43:59 -0500

    wip

---
+++ app/Models/Speaking.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Sushi\Sushi;
+use Symfony\Component\Finder\Finder;
+
+class Speaking extends Model
+{
+    use Sushi;
+
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime',
+        ];
+    }
+
+    public function getRows(): array
+    {
+        return $this->getSpeakingFiles()
+            ->map(fn ($file) => $this->parseFrontMatter($file))
+            ->pipe(fn ($collection) => $this->appendTranscript($collection))
+            ->sortBy('published_at')
+            ->values()
+            ->toArray();
+    }
+
+    protected function getSpeakingFiles(): Collection
+    {
+        return Collection::make(
+            Finder::create()
+                ->files()
+                ->in(resource_path('content/speaking'))
+                ->name('*.md'),
+        );
+    }
+
+    protected function parseFrontMatter(string $file): array
+    {
+        $document = YamlFrontMatter::parseFile($file);
+
+        return array_merge($document->matter(), [
+            'slug' => basename($file, '.md'),
+        ]);
+    }
+
+    protected function appendTranscript(Collection $items): Collection
+    {
+        return $items->map(function ($item) {
+            $path = resource_path("content/speaking/transcripts/{$item['slug']}.txt");
+            $transcript = File::exists($path) ? $path : null;
+
+            return array_merge($item, [
+                'transcript' => $transcript,
+            ]);
+        });
+    }
+}
