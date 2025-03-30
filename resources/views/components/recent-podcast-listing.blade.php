f324ccfac5d888ed588c3f692b7df048d9c940fd
ahinkle <ahinkle10@gmail.com>
2025-03-29 22:18:57 -0500

    wip

--- resources/views/components/recent-podcast-listing.blade.php
+++ resources/views/components/recent-podcast-listing.blade.php
@@ -1,11 +1,11 @@
 <div class="grid gap-12 gap-y-4 py-4 md:gap-y-8 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-12">
-    @foreach ($podcasts as $podcast)
-        <a class="group rounded-lg bg-gray-900 p-4" href="{{ url("/speaking/{$podcast->slug}") }}">
+    @foreach ($podcasts as $episode)
+        <a class="group rounded-lg bg-gray-900 p-4" href="{{ url("/speaking/{$episode->slug}") }}">
             <div class="grid grid-cols-1 gap-4">
                 <div class="col-span-1">
                     <div class="relative flex items-center justify-between">
-                        @if ($podcast->video_url)
-                            <img src="{{ $podcast->video_thumbnail }}" alt="{{ $podcast->title }}"
+                        @if ($episode->video_url)
+                            <img src="{{ $episode->video_thumbnail }}" alt="{{ $episode->title }}"
                                 class="h-full w-full rounded-lg object-cover">
                         @endif
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
@@ -18,10 +18,10 @@
                 <div class="col-span-1">
                     <div class="flex h-full flex-col">
                         <div class="h-16 flex-none overflow-hidden">
-                            <p class="line-clamp-2 text-lg font-semibold text-white">{{ $podcast->title }}</p>
+                            <p class="line-clamp-2 text-lg font-semibold text-white">{{ $episode->title }}</p>
                         </div>
                         <div class="mt-3 flex-grow">
-                            <p class="line-clamp-3 text-sm text-gray-300">{{ str($podcast->summary)->words(20) }}
+                            <p class="line-clamp-3 text-sm text-gray-300">{{ str($episode->summary)->words(20) }}
                             </p>
                         </div>
                         <div class="mt-4 flex-none">
@@ -33,7 +33,7 @@
                                             d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L64 64C28.7 64 0 92.7 0 128l0 16 0 48L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-256 0-48 0-16c0-35.3-28.7-64-64-64l-40 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L152 64l0-40zM48 192l352 0 0 256c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256z" />
                                     </svg>
                                     <time
-                                        datetime="{{ $podcast->published_at->format('Y-m-d') }}">{{ $podcast->published_at->format('M j, Y') }}</time>
+                                        datetime="{{ $episode->published_at->format('Y-m-d') }}">{{ $episode->published_at->format('M j, Y') }}</time>
                                 </span>
                                 <span class="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +42,7 @@
                                         <path fill-rule="evenodd"
                                             d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm0 18c-4.418 0-8-3.582-8-8s3.582-8 8-8 8 3.582 8 8-3.582 8-8 8zm-1-13a1 1 0 0 1 2 0v5a1 1 0 0 1-2 0V5z" />
                                     </svg>
-                                    {{ $podcast->duration_mmss }}
+                                    {{ $episode->duration_mmss }}
                                 </span>
                             </p>
                         </div>
