96122c6ad29f98ce23668f45ba1646171a6ccc3d
Andy Hinkle <ahinkle10@gmail.com>
2025-10-27 09:37:33 -0400

    Tip from JBeggs 🔥

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -9,7 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="dark">
+<body class="dark antialiased">
     @env('local')
         <x-dev.dev-toolbar />
     @endenv
